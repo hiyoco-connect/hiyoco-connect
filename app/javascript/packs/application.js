853b9d55d210944a37685124e5c12f68946439fc
runteq-profile <octpsubaru@icloud.com>
2022-02-08 13:43:00 +0900

    add: bootstrap,popper.jsを追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-
+mport "bootstrap"
+import "../stylesheets/application"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
