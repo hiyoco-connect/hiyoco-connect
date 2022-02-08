3d2a4b565738eab1cbe91d47804553150d86eb6f
runteq-profile <octpsubaru@icloud.com>
2022-02-08 14:54:10 +0900

    chore: turbolinksの削除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,11 +4,9 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-mport "bootstrap"
+import "bootstrap"
 import "../stylesheets/application"
 Rails.start()
-Turbolinks.start()
 ActiveStorage.start()
