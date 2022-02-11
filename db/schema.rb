008e77ef6d7868a9ceddf121ff1b73d230784069
ohno <ddeeppaa.gogo@gmail.com>
2022-02-11 14:22:50 +0900

    feat: 外部認証用テーブル作成。db:migrate実行 #23

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_11_045050) do
+ActiveRecord::Schema.define(version: 2022_02_11_052017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authentications", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "provider", null: false
+    t.string "uid", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["provider", "uid"], name: "index_authentications_on_provider_and_uid"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "github_name"
