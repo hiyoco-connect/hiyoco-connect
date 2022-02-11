0b07006fd61455141d440d3ff0d8845fe9f83a66
ohno <ddeeppaa.gogo@gmail.com>
2022-02-11 13:52:36 +0900

    feat: sorcery install #23

---
+++ db/migrate/20220211045050_sorcery_core.rb
@@ -0,0 +1,11 @@
+class SorceryCore < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :email,            null: false, index: { unique: true }
+      t.string :crypted_password
+      t.string :salt
+
+      t.timestamps                null: false
+    end
+  end
+end
