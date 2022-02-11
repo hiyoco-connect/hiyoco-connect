777d20d4352e612d29c3900b3432a9b77e49730e
ohno <ddeeppaa.gogo@gmail.com>
2022-02-11 13:56:52 +0900

    feat: マイグレーションファイル変更。db:migrate実行 #23

--- db/migrate/20220211045050_sorcery_core.rb
+++ db/migrate/20220211045050_sorcery_core.rb
@@ -1,6 +1,7 @@
 class SorceryCore < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
+      t.string :github_name
       t.string :email,            null: false, index: { unique: true }
       t.string :crypted_password
       t.string :salt
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id               :bigint           not null, primary key
+#  crypted_password :string
+#  email            :string           not null
+#  github_name      :string
+#  salt             :string
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email  (email) UNIQUE
+#
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
