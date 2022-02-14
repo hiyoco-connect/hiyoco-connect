da063a6116f73e6d1eef49d29b46551552adf718
mayu-tateno <87419889+mayu-tateno@users.noreply.github.com>
2022-02-14 18:44:21 +0900

    feat: profilesテーブルにusersテーブルの外部キー制約をつけた#25

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -19,6 +19,15 @@
 #  twitter_account   :string
 #  created_at        :datetime         not null
 #  updated_at        :datetime         not null
+#  user_id           :bigint
+#
+# Indexes
+#
+#  index_profiles_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
 #
 class Profile < ApplicationRecord
   belongs_to :user
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_14_051030) do
+ActiveRecord::Schema.define(version: 2022_02_14_084018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 2022_02_14_051030) do
     t.string "avatar"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -54,4 +56,5 @@ ActiveRecord::Schema.define(version: 2022_02_14_051030) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "profiles", "users"
 end
