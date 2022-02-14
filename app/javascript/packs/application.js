4bee54189f505872aa5b530deb33962901d527d2
ohno <ddeeppaa.gogo@gmail.com>
2022-02-14 14:43:15 +0900

    style: style fixed #23

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,6 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
 import "../stylesheets/application"
-import '@fortawesome/fontawesome-free/js/all';
+import '@fortawesome/fontawesome-free/js/all'
 Rails.start()
 ActiveStorage.start()
