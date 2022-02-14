2f3cc072f5861417aa10cc01a85092d51ba30025
ohno <ddeeppaa.gogo@gmail.com>
2022-02-14 14:21:02 +0900

    feat: GitHubログインに必要な画像カラムをユーザーモデルに追加 #23

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,13 +2,14 @@
 #
 # Table name: users
 #
-#  id               :bigint           not null, primary key
-#  crypted_password :string
-#  email            :string           not null
-#  github_name      :string
-#  salt             :string
-#  created_at       :datetime         not null
-#  updated_at       :datetime         not null
+#  id                :bigint           not null, primary key
+#  crypted_password  :string
+#  email             :string           not null
+#  github_name       :string
+#  remote_avatar_url :string
+#  salt              :string
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
 #
 # Indexes
 #
