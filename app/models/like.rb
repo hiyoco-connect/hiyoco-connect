3a2504026eab3e3a4d5f8ac6b40f5f3f4c718370
ohno <ddeeppaa.gogo@gmail.com>
2022-02-19 19:41:47 +0900

    feat: Likeモデル作成。db:migrate実行 #27

---
+++ app/models/like.rb
@@ -0,0 +1,25 @@
+# == Schema Information
+#
+# Table name: likes
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  profile_id :bigint           not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_likes_on_profile_id              (profile_id)
+#  index_likes_on_user_id                 (user_id)
+#  index_likes_on_user_id_and_profile_id  (user_id,profile_id) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (profile_id => profiles.id)
+#  fk_rails_...  (user_id => users.id)
+#
+class Like < ApplicationRecord
+  belongs_to :user
+  belongs_to :profile
+end
