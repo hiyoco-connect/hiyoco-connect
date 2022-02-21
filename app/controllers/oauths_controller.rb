9871e86fd98d594163ca9c4e64aabd12a2b6fe8e
mayu-tateno <87419889+mayu-tateno@users.noreply.github.com>
2022-02-21 12:38:19 +0900

    fix: oauthコントローラーの記述をrubocopに指摘されないよう変更#11

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -7,7 +7,8 @@ class OauthsController < ApplicationController
 
   def callback
     provider = auth_params[:provider]
-    if @user = login_from(provider)
+    @user = login_from(provider)
+    if @user
       redirect_to root_path, success: t('.success')
     else
       begin
