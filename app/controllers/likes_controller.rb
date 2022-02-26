80758bce2de5457eb820c1cd990754ef54807747
ohno <ddeeppaa.gogo@gmail.com>
2022-02-27 00:34:15 +0900

    refactor: i18n記載,rubocop実行 #27

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,12 +2,12 @@ class LikesController < ApplicationController
   def create
     @profile = Profile.find(params[:profile_id])
     current_user.like(@profile)
-    redirect_to profile_path(@profile), success: 'いいねしました' #仮のリダイレクト
+    redirect_to profile_path(@profile), success: t('.success')
   end
 
   def destroy
     @profile = Profile.find(params[:profile_id])
     current_user.unlike(@profile)
-    redirect_to profile_path(@profile), success: 'いいねを取り消しました' #仮のリダイレクト
+    redirect_to profile_path(@profile), success: t('.success')
   end
 end
