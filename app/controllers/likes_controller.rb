cebe0f47aabb848b36c1e8489af89f0d838972c1
ohno <ddeeppaa.gogo@gmail.com>
2022-02-27 19:27:47 +0900

    fix: フラッシュメッセージ削除。いいね済みの背景色変更 #27

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,12 +2,12 @@ class LikesController < ApplicationController
   def create
     @profile = Profile.find(params[:profile_id])
     current_user.like(@profile)
-    redirect_to profile_path(@profile), success: t('.success')
+    redirect_to profile_path(@profile)
   end
 
   def destroy
     @profile = Profile.find(params[:profile_id])
     current_user.unlike(@profile)
-    redirect_to profile_path(@profile), success: t('.success')
+    redirect_to profile_path(@profile)
   end
 end
