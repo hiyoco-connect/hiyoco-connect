8cf29382dc68c3b507bdaedf3defff69e276cbfb
mayu-tateno <87419889+mayu-tateno@users.noreply.github.com>
2022-03-06 15:02:18 +0900

    feat: アップロード時のリサイズ方法を設定#65

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,7 +1,7 @@
 class AvatarUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -23,7 +23,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process scale: [200, 300]
+  process resize_to_fill: [512, 512, 'center']
   #
   # def scale(width, height)
   # do something
